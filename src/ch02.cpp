01e9e42f09d90024bd7add7631e39f2bd89a59b2
Jooh <mnh513@naver.com>
2023-11-09 13:45:48 +0900

    ch02 ch02_answer added

---
+++ src/ch02.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+GLFWwindow* window;
+const int WINDOW_WIDTH  = 1024;
+const int WINDOW_HEIGHT = 768;
+
+unsigned int shaderProgram;
+unsigned int VAO;
+
+void window_size_callback(GLFWwindow* window, int width, int height)
+{
+}
+
+void processInput(GLFWwindow* window)
+{
+}
+
+int init()
+{
+    return -1;
+}
+
+void render()
+{
+   
+}
+
+void update()
+{
+	
+}
+
+int main(void)
+{
+    return -1;
+}
--- src/ch02_answer.cpp
+++ src/ch02_answer.cpp
@@ -1,7 +1,3 @@
-/** 
- * Copyright (C) 2018 Tomasz Ga�aj
- **/
-
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -9,6 +5,9 @@
 GLFWwindow* window;
 const int WINDOW_WIDTH  = 1024;
 const int WINDOW_HEIGHT = 768;
+
+unsigned int shaderProgram;
+unsigned int VAO;
 
 void window_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -57,8 +56,6 @@ int init()
     glClearColor(0.6784f, 0.8f, 1.0f, 1.0f);
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    glEnable(GL_DEPTH_TEST);
-
     return true;
 }
 
@@ -67,10 +64,71 @@ void render()
     // rendering commands here
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // state-setting function
     glClear(GL_COLOR_BUFFER_BIT); // state-using function
+
+	// rendering triangle
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 void update()
 {
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// 나중에는 파일 로드 형식으로 변경
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\0";
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glUseProgram(shaderProgram);
+
+	glGenVertexArrays(1, &VAO);
+	// ..:: Initialization code (done once (unless your object frequently changes)) :: ..
+	// 1. bind Vertex Array Object
+	glBindVertexArray(VAO);
+	// 2. copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. then set our vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
